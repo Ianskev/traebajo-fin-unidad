ae30076ab76b45acae55995f878333d055b2e6ab
Ianskev <112659831+Ianskev@users.noreply.github.com>
2022-11-06 19:31:58 -0500

    Update tarea1.py
    - NUEVAS FUNCIONALIDADES Y ACTUALIZACIÓN DE CODIGO

--- tarea1.py
+++ tarea1.py
@@ -1,40 +1,50 @@
-import requests
+import os
+import time
 import csv
-class Libro:
-    def __init__(self, id, titulo, genero, isbn, editorial, autor):
-        self.__id = id
-        self.__titulo = titulo
-        self.__genero = genero
-        self.__isbn = isbn
-        self.__editorial = editorial
-        self.__autor = autor
 
+BOLD = '\033[1m'
+BLACK = '\033[30m'
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\033[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[0m'
+
+def printOptions():
+    print(BOLD + BLUE+"|--------------------------------------------------|")
+    print("|-----------------MENU DE OPCIONES-----------------|")
+    print("|--------------------------------------------------|\n"+ RESET)
+    print(BOLD + RED +'[1]'+RESET +' Leer archivo .csv o .txt')
+    print(BOLD + RED +'[2]'+ RESET + ' Listar libros ')
+    print(BOLD + RED +'[3]'+ RESET +' Agregar Libros')
+    print(BOLD + RED +'[4]'+ RESET +' Eliminar Libros')
+    print(BOLD + RED +'[5]'+ RESET +' Buscar libros por ISBN o Titulo')
+    print(BOLD + RED +'[6]'+ RESET +' Ordenar los libros por titulos')
+    print(BOLD + RED +'[7]'+ RESET +' Buscar libros editorial,genero o autor')
+    print(BOLD + RED +'[8]'+ RESET +' Buscar libro por número de autores')
+    print(BOLD + RED +'[9]'+ RESET + ' Editar libro')
+    print(BOLD + RED +'[10]'+ RESET +' Guardar libro en .csv o .txt')
+    print(BOLD + RED +'[11]'+ RESET +' Salir'+RESET)
+    print("-" * 20)
+
+class Book:
+    def __init__(self, id:str, title:str, genre:str, ISBN:str, editorial:str, autors:str):
+        self.__id = id
+        self.__title = title
+        self.__genre = genre
+        self.__ISBN = ISBN
+        self.__editorial = editorial
+        self.__autors = autors
     def get_atributos(self):
-        return self.__id, self.__titulo, self.__genero, self.__isbn, self.__editorial, self.__autor
+        return self.__id,self.__title,self.__genre,self.__ISBN,self.__editorial,self.__autors
 
-    def set_atributos(self,id, titulo, genero, isbn, editorial, autor):
+    def set_atributos(self, id,tittle,genre,ISBN,editorial,autors):
         self.__id = id
-        self.__titulo = titulo
-        self.__genero = genero
-        self.__isbn = isbn
+        self.__title = tittle
+        self.__genre = genre
+        self.__ISBN = ISBN
         self.__editorial = editorial
-        self.__autor = autor
-    
-    def __del__(self):
-        return None
-
-lista_de_libros = []
-def leer_archivos(nombre_archivo):
-    with open(nombre_archivo, newline='', encoding='utf-8') as f:
-        rows = csv.DictReader(f)
-        for r in rows:
-            lista_de_libros.append(r)
-def lista_libr():
-    for elem in lista_de_libros:
-        for k,v in elem.items():
-            print(k, v)
-def main():
-    print("Bienvenidos")
-    print("Opcion 3")
-leer_archivos("tarea_final.txt")
-lista_libr()
+        self.__autors = autors
